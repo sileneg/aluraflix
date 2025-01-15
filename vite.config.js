b53797d7d53ccd4192305b61613bb1b163010da0
sileneg <silenegonzalezquiroz@gmail.com>
2025-01-15 01:34:37 -0500

    Configuración para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/aluraflix/' : '/', // 'build' es para producción
   plugins: [react()],
   server: {
     port: 3000,
@@ -11,4 +12,4 @@ export default defineConfig({
       '@': '/src',
     },
   },
-});
+}));
