86f49bc2e66dab2f0740ba4494b47afc798c4d81
sileneg <silenegonzalezquiroz@gmail.com>
2025-01-15 06:57:55 -0500

    Fix vite.config.js for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 const App = () => {
-  const basename = process.env.NODE_ENV === 'production' ? '/aluraflix' : '/';
+  const basename = process.env.NODE_ENV === 'production' && import.meta.env.VITE_DEPLOY_TARGET === 'github' ? '/aluraflix' : '/';
 
   return (
     <Router basename={basename}>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Determinar la base dinámicamente
+const base = process.env.VITE_DEPLOY_TARGET === 'github' ? '/aluraflix/' : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
 });
